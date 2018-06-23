996ccdffdea2f6468138e410e343630dd692b1f6
tslindner <timothyslindner@gmail.com>
2018-06-23 13:37:57 -0400

    add mongodb functionality

--- WIP dirs/Tim/app.py
+++ WIP dirs/Tim/app.py
@@ -1,8 +1,12 @@
+from bson import Binary, Code, json_util
+from bson.json_util import dumps
 from flask import Flask, render_template, redirect
 from flask_restful import Resource, Api
 
+import json
 import pandas as pd
 import pymongo
+
 
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
@@ -13,22 +17,23 @@ collection = db.data
 app = Flask(__name__)
 api = Api(app)
 
-real_estate_list = pd.read_csv("input_data/real_estate.csv")
-real_estate_list = real_estate_list.rename(index=str, columns={"$/SQUARE FEET": "SQUARE FEET 2", "URL (SEE http://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)": "URL"})
-real_estate_list = real_estate_list.to_dict(orient="records")
 
+@app.route("/data")
+def scraper():
 
-# @app.route("/scrape")
-# def scraper():
+    collection.drop()
 
-#     collection.drop()
+    real_estate_list = pd.read_csv("input_data/real_estate.csv")
+    real_estate_list = real_estate_list.rename(index=str, columns={"$/SQUARE FEET": "SQUARE FEET 2", "URL (SEE http://www.redfin.com/buy-a-home/comparative-market-analysis FOR INFO ON PRICING)": "URL"})
+    real_estate_list = real_estate_list.to_dict(orient="records")
 
-#     results = scrape()
+    for doc in real_estate_list:
+        collection.insert_one(doc)
     
-#     collection.insert_one(results)
+    # collection.insert_many(real_estate_list)
 
-#     return redirect("..")
-    
+    return redirect("..")
+
 @app.route("/")
 def home():
     
@@ -39,7 +44,15 @@ def home():
 class input_data(Resource):
     def get(self):
 
-        return real_estate_list
+
+        items = collection.find()
+        return [json.loads(json.dumps(item, indent=4, default=json_util.default))
+                for item in items]
+
+
+        # data = collection.find()
+
+        # return data
 
 
         # for real_estate_document in real_estate_list:
--- WIP dirs/Tim/static/js/logic.js
+++ WIP dirs/Tim/static/js/logic.js
@@ -1,21 +1,70 @@
 
 
-// Create a map object
-var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
-  });
+// // Create a map object
+// var myMap = L.map("map", {
+//     center: [37.09, -95.71],
+//     zoom: 5
+//   });
   
-  // Add a tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
-    "T6YbdDixkOBWH_k9GbS8JQ"
-  ).addTo(myMap);
+//   // Add a tile layer
+//   L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+//     "access_token=pk.eyJ1IjoidHNsaW5kbmVyIiwiYSI6ImNqaWNhdTFzdzFuam4za21sc3ZiMmN5bDEifQ.5Il8Y1QtwyMFWCa1JkDY_Q"
+//   ).addTo(myMap);
 
-  for (var i = 0; i < cities.length; i++) {
-    var city = cities[i];
-    L.marker(city.location)
-      .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
-      .addTo(myMap);
-  }
+//   for (var i = 0; i < cities.length; i++) {
+//     var city = cities[i];
+//     L.marker(city.location)
+//       .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+//       .addTo(myMap);
+//   }
+
+
+
+
+
+
+d3.json('http://localhost:5000/resource', function(real_estate_data) {
+  console.log(real_estate_data);
+  var data = real_estate_data;
+  console.log(data)
+});
+
+
+
+
+d3.select("#test").selectAll("div")
+  .data(data)
+  .enter()
+  .append("div")
+  .classed("col-md-4", true)
+  .html(function(d) {
+    return `<p> ${d.ADDRESS}</p>`;
+  });
+
+
+
+
+
+
+
+
+
+// // Create a map object
+// var myMap = L.map("map", {
+//     center: [37.09, -95.71],
+//     zoom: 5
+//   });
+  
+//   // Add a tile layer
+//   L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+//     "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
+//     "T6YbdDixkOBWH_k9GbS8JQ"
+//   ).addTo(myMap);
+
+//   for (var i = 0; i < cities.length; i++) {
+//     var city = cities[i];
+//     L.marker(city.location)
+//       .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Population " + city.population + "</h3>")
+//       .addTo(myMap);
+//   }
   
